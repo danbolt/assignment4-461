b381f19f410f87c00877537fd0c24e37a11350a6
Daniel Savage <dansvg@gmail.com>
2013-04-02 16:09:02 -0700

    fleshed out window

--- ConferenceClient.java
+++ ConferenceClient.java
@@ -1,16 +1,55 @@
-//
+import javax.swing.ButtonGroup;
+import javax.swing.JButton;
+import javax.swing.JRadioButton;
+import javax.swing.JTabbedPane;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JFrame;
+import javax.swing.JComponent;
+import javax.swing.JTextField;
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.FileDialog;
+import java.awt.FlowLayout;
+import java.awt.Frame;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
 
-public class ConferenceClient
+import javax.media.ControllerListener;
+import javax.media.MediaLocator;
+import javax.media.Player;
+import javax.media.rtp.ReceiveStream;
+
+public class ConferenceClient extends JFrame implements ActionListener
 {
 	ClientBroadcaster stream = null;
 
-	public ConferenceClient()
+	public ConferenceClient ()
+	{
+		super("Hermes");
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		this.setSize(300, 300);
+		initalize();
+	}
+	
+	public void initalize ()
+	{
+		this.setVisible(true);
+	}
+	
+	public void actionPerformed (ActionEvent e)
 	{
 		//
 	}
 
 	public static void main (String[] args)
 	{
-		System.out.println("Hello world!");
+		new ConferenceClient();
 	}
 }
