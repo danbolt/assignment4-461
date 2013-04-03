daebf7a60fb1516f8322c40eb87ffaedb1d72257
Daniel Savage <dansvg@gmail.com>
2013-04-03 15:17:08 -0700

    clients are talking to each other, but no RTP

--- ConferenceClient.java
+++ ConferenceClient.java
@@ -28,26 +28,53 @@ import javax.media.rtp.ReceiveStream;
 public class ConferenceClient extends JFrame implements ActionListener
 {
 	ClientBroadcaster stream = null;
+	ClientReceiver receiver = null;
 	
 	public JComponent basePanel;
+	
+	/* FOR TEMPORARY TESTING REMOVE THIS LATER */
+	
+	private boolean sending;
+	
+	/* END OF TEMP TESTING CODE (LOLOLOL AGILE SCRUM PROTOTYPING ETC) */
 
-	public ConferenceClient ()
+	public ConferenceClient (boolean isSending)
 	{
 		super("Hermes");
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		sending = isSending;
 
 		this.setSize(300, 300);
 		initalize();
 	}
-	
+
 	public void initalize ()
 	{
 		basePanel = new JPanel(false);
 		basePanel.setLayout(new BorderLayout());
 		add(basePanel);
 
-		stream = new ClientBroadcaster(new MediaLocator("file:samples/atime.mov"), "224.0.0.100", 9000, 0.5f, this);
-		stream.start();
+		if (sending)
+		{
+			stream = new ClientBroadcaster(new MediaLocator("file:samples/atime.mov"), "224.0.0.100", 9000, 0.5f, this);
+			stream.start();
+		}
+		else
+		{
+			String[] sessions = new String[2];
+			sessions[0] = "224.0.0.100" + '/' + 9000;
+			sessions[1] = "224.0.0.100" + '/' + (9000+2);
+			
+			int bufferSize = 350;
+
+			receiver = new ClientReceiver(sessions, this, bufferSize);
+			if (!(receiver.initalize()))
+			{
+				System.out.println("FAILED to initialize the sessions.");
+				System.exit(-1);
+			}
+		}
 
 		this.setVisible(true);
 	}
@@ -59,6 +86,13 @@ public class ConferenceClient extends JFrame implements ActionListener
 
 	public static void main (String[] args)
 	{
-		new ConferenceClient();
+		if ("send".equals(args[0]))
+		{
+			new ConferenceClient(true);
+		}
+		else if ("recv".equals(args[0]))
+		{
+			new ConferenceClient(false);
+		}
 	}
 }
