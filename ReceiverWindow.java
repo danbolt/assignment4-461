53c04bea389078e48ce4d6ddf349f9efa998809c
Daniel Savage <dansvg@gmail.com>
2013-04-05 14:56:35 -0700

    added a receiver window for each receive

---
+++ ReceiverWindow.java
@@ -0,0 +1,41 @@
+import javax.swing.ButtonGroup;
+import javax.swing.JButton;
+import javax.swing.JRadioButton;
+import javax.swing.JTabbedPane;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JFrame;
+import javax.swing.JComponent;
+import javax.swing.JTextField;
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.FileDialog;
+import java.awt.FlowLayout;
+import java.awt.Frame;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+
+public class ReceiverWindow extends JFrame implements ActionListener
+{
+	public JComponent basePanel;
+	
+	public ReceiverWindow()
+	{
+		super("Receiver Window");
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		this.setSize(300, 300);
+		// init function?
+		this.setVisible(true);
+	}
+	
+	public void actionPerformed (ActionEvent e)
+	{
+		//
+	}
+}
